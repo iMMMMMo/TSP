f5f98f08da2c9ab44bba21c8d5563f7b5c1e6e57
adamwebmaker <adam612@poczta.onet.pl>
2022-12-05 00:25:47 +0100

    blad przy new_point #110

--- main.py
+++ main.py
@@ -6,10 +6,8 @@ import time
 def generate_coords(choice):
     coords = []
     if choice == 0:
-        f = open(f"dane/dane20.txt", "w")
         print("Podaj ilosc wierzcholkow: ", end="")
         n = int(input())
-        f.write(f"{str(n)}\n")
         print("Podaj zakres rozmieszczenia punktow - od a do b wlacznie")
         print("Podaj a: ", end="")
         a = int(input())
@@ -20,7 +18,6 @@ def generate_coords(choice):
             tmp = [random.randint(a, b), random.randint(a, b)]
             if tmp not in coords:
                 coords.append(tmp)
-                f.write(f"{str(tmp[0])} {str(tmp[1])}\n")
     elif choice == 1:
         print("Podaj ilosc wierzcholkow: ", end="")
         n = int(input())
@@ -66,6 +63,19 @@ def create_distance_matrix(n, coords):
     return matrix
 
 
+def pheromones_graph(matrix):
+    new = []
+    for i in range(len(matrix)):
+        temp = []
+        for j in range(len(matrix)):
+            if i == j:
+                temp.append(0)
+            else:
+                temp.append(1)
+        new.append(temp)
+    return new
+
+
 def TSP(n, vis, curr_point, cnt):
     print(curr_point, end=", ")
     vis[curr_point] = 1
@@ -84,6 +94,52 @@ def TSP(n, vis, curr_point, cnt):
     TSP(n, vis, new_point, cnt)
 
 
+def tspColony(n, vis, curr_point, cnt,path, phero):
+    print(curr_point, end=", ")
+    path.append(curr_point)
+    vis[curr_point] = 1
+    cnt += 1
+    if cnt == n:
+        print()
+        cost.append(distances[0][curr_point])
+        return path
+
+    shortest = float('inf')
+    for i in range(n):
+        if i!=curr_point and vis[i] == 0:
+            new_point = ant(distances,phero,i,vis)
+            shortest = distances[curr_point][new_point]
+    cost.append(shortest)
+    tspColony(n, vis, new_point, cnt,path,phero)
+
+
+def ant(dist, phero,position, vis):
+    chances=[]
+    cumulative_sum=[]
+    for i in range(len(dist)):
+        if i == position or i in vis:
+            pass
+        else:
+            nominator = (phero[position][i] * (1/dist[position][i]))
+            denominator = 0
+            for j in range(len(dist)):
+                if j == position:
+                    pass
+                else:
+                    denominator += ((phero[position][j] * (1/dist[position][j])))
+            chances.append(nominator/denominator)
+            #print(nominator,denominator,nominator/denominator)
+    cumulative_sum.append(sum(chances))
+    for i in range(len(chances)-1):
+        cumulative_sum.append(cumulative_sum[i]-chances[i])
+        #print(cumulative_sum[i])
+    choose = random.random()
+    for i in range(len(cumulative_sum)):
+        if cumulative_sum[len(cumulative_sum)-i-1]>=choose:
+            #print("dla ",i," wynik to ",cumulative_sum[len(cumulative_sum)-i-1],choose)
+            return i
+
+
 print("Wybierz jedna z opcji:")
 print("0 - jesli program ma sam generowac wspolrzedne")
 print("1 - jesli chcesz sam wpisac wspolrzedne")
@@ -92,7 +148,9 @@ choice = int(input())
 
 coords = generate_coords(choice)
 distances = create_distance_matrix(len(coords), coords)
+pheromones = pheromones_graph(distances)
 
+path = []
 cost = []
 visited = [0 for _ in range(len(coords))]
 start = time.time()
@@ -100,3 +158,17 @@ TSP(len(coords), visited, 0, 0)
 end = time.time()
 print(f"Koszt przejscia: {sum(cost)}")
 print(f"Czas egzekucji algorytmu zachlannego: {(end - start)}")
+start = time.time()
+tspColony(len(coords), visited, 0, 0, path, pheromones)
+#print(ant(distances,pheromones,0,visited))
+end = time.time()
+print(f"Koszt przejscia: {sum(cost)}")
+print(f"Czas egzekucji algorytmu zachlannego: {(end - start)}")
+
+#ant(distances,pheromones,0)
+
+# for x in distances:
+#     print(*x)
+#
+# for x in pheromones:
+#     print(*x)
